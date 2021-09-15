a6ae4aa6dc26a8664038a68e8202434c0dacce34
George <goshatravin@gmail.com>
2021-09-15 19:20:42 +0300

    push

--- src/components/textMessage.tsx
+++ src/components/textMessage.tsx
@@ -1,56 +1,50 @@
 import React from 'react'
 import Styled from 'styled-components'
-import { Avatar } from 'react95'
 
 export type IDate = {
-    text: string
-    id: number
-}
-const Wrapper = Styled.div`
-    width: 100%;
-    text-align: right;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;`
-const MessageDiv: any = Styled.div`
-    display: flex;
-    padding: ${(props: any) =>
-        props.from ? '4px 15px 4px 5px' : '4px 5px 4px 15px'};
-    background: white;
-    margin: 10px;
-    font-size: 1rem;
-    border-style: solid;
-    border-width: 2px;
-    border-color: rgb(223, 223, 223) rgb(10, 10, 10) rgb(10, 10, 10) rgb(223, 223, 223);
-    box-sizing: border-box;
-    background: rgb(198, 198, 198);
-    color: rgb(10, 10, 10);
-    width: fit-content;
-    text-align: ${(props: any) => (props.from ? 'left' : 'right')};
-    align-self: ${(props: any) => (props.from ? 'flex-start' : 'flex-end')};`
-const AvatarWrapper = Styled.div`
-    margin-right: 10px;
-    p{
-        font-size: 13px;
+    data: {
+        message: string
+        name: string
+        time: string
+        id: number
+        from: boolean
     }
-`
+}
 
-const Message = ({ data }: any) => (
-    <Wrapper>
-        <MessageDiv from={data.from} key={data.id}>
-            {data.from && (
-                <AvatarWrapper>
-                    <p>Gosha</p>
-                    <Avatar square size={30}>
-                        <span role="img" aria-label="🚀">
-                            🚀
-                        </span>
-                    </Avatar>
-                </AvatarWrapper>
-            )}
-            {data.text}
-        </MessageDiv>
-    </Wrapper>
-)
+const Wrap: any = Styled.div`
+border-style: solid;
+border-width: 2px;
+border-color: rgb(223, 223, 223) rgb(10, 10, 10) rgb(10, 10, 10) rgb(223, 223, 223);
+box-sizing: border-box;
+color: rgb(10, 10, 10);
+width: fit-content;
+margin: 10px;
+padding: ${(props: any) => (props.from === 'true' ? '4px 10px 4px 5px' : '4px 5px 4px 10px')};
+background: ${(props: any) => (props.from === 'true' ? 'rgb(198, 198, 198);' : 'white')};
+text-align: ${(props: any) => (props.from === 'true' ? 'left' : 'left')};
+align-self: ${(props: any) => (props.from === 'true' ? 'flex-start' : 'flex-end')};`
+const MessageDiv: any = Styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-end;`
+const Time: any = Styled.div`
+font-size: 10px;
+text-align: ${(props: any) => (props.from === 'true' ? 'right' : 'left')};`
+const Name = Styled.div`
+font-size: 12px;
+width: 100%;`
+
+const Message = ({ data }: IDate) => {
+    const { id, from, message, time, name } = data
+    return (
+        <Wrap key={id} from={from?.toString()}>
+            <MessageDiv>
+                {from && <Name>{name || 'Piter'}</Name>}
+                {message}
+                <Time from={from?.toString()}>{time}</Time>
+            </MessageDiv>
+        </Wrap>
+    )
+}
 
 export default Message
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Styled from 'styled-components'
-import { TextField, Button } from 'react95'
-import Message, { IDate } from '../components/textMessage'
+import { TextField, Button, Hourglass } from 'react95'
+import Message from '../components/textMessage'
 
 const Wrapper = Styled.div`
     width: 100%;
@@ -20,49 +20,47 @@ const Content = Styled.div`
      flex: 1;
      overflow-y: scroll;
 `
+const LoadingWrap = Styled.div`
+    padding: 0 9px;
+`
 
 const messages = [
     {
         id: 0,
-        text: 'lorem18 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1lorem 1',
-        timestamp: '11/22/11',
+        message:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        time: '14:24',
         from: true,
+        name: 'Gosha',
     },
-    { id: 1, text: 'lorem 2', timestamp: '11/22/11' },
-    { id: 2, text: 'lorem 3', timestamp: '11/22/11', from: true },
-    { id: 3, text: 'lorem 4', timestamp: '11/22/11' },
-    { id: 4, text: 'lorem 1', timestamp: '11/22/11' },
-    { id: 5, text: 'lorem 2', timestamp: '11/22/11' },
-    { id: 6, text: 'lorem 3', timestamp: '11/22/11', from: true },
-    { id: 7, text: 'lorem 4', timestamp: '11/22/11' },
-    { id: 8, text: 'lorem 1', timestamp: '11/22/11', from: true },
-    { id: 9, text: 'lorem 2', timestamp: '11/22/11' },
-    { id: 10, text: 'lorem 3', timestamp: '11/22/11', from: true },
-    { id: 11, text: 'lorem 4', timestamp: '11/22/11', from: true },
-    { id: 12, text: 'lorem 1', timestamp: '11/22/11', from: true },
-    { id: 13, text: 'lorem 2', timestamp: '11/22/11', from: true },
-    { id: 14, text: 'lorem 3', timestamp: '11/22/11', from: true },
-    { id: 15, text: 'lorem 4', timestamp: '11/22/11' },
-    { id: 16, text: 'lorem 1', timestamp: '11/22/11' },
-    {
-        id: 17,
-        text: 'lorem 2lorem 2lorem 2lorem 2lorem 2lorem 2lorem 2',
-        timestamp: '11/22/11',
-    },
-    { id: 18, text: 'lorem 3', timestamp: '11/22/11' },
-    { id: 19, text: 'lorem 4', timestamp: '11/22/11' },
 ]
 
 // @ts-ignore
-const mapper = (item: IDate) => <Message data={item} />
+const mapper = (item) => <Message key={item.id} data={item} />
 
 const Home = () => {
     const [text, setText] = useState<String>('')
+    const [isSending, setIsSending] = useState<boolean>(false)
+    const sendMessage = (e: any) => {
+        // eslint-disable-next-line no-new
+        new Promise((resolve) => {
+            setIsSending(true)
+            setText('')
+            setTimeout(() => {
+                resolve(setIsSending(false))
+            }, 3000)
+        })
+        if (e.keyCode === 13) {
+            console.log('123')
+        }
+    }
+
     return (
         <Wrapper>
             <Content>{messages.map(mapper)}</Content>
             <WrapperInput>
                 <TextField
+                    onKeyDown={(e: any) => e.keyCode === 13 && sendMessage(e)}
                     name="email"
                     value={text}
                     placeholder="New message..."
@@ -71,7 +69,15 @@ const Home = () => {
                     }}
                     fullWidth
                 />
-                <Button>Send</Button>
+                <Button disabled={text.length === 0 || isSending} onClick={sendMessage}>
+                    {isSending ? (
+                        <LoadingWrap>
+                            <Hourglass size={20} />
+                        </LoadingWrap>
+                    ) : (
+                        'Send'
+                    )}
+                </Button>
             </WrapperInput>
         </Wrapper>
     )
